e27247270a429c99222ffcd78f19c10ce74de350
Fabio Stori <f196631@dac.unicamp.br>
2020-07-24 19:29:21 -0300

    initial commit

---
+++ Chapter 1/project-template/src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-server",
+  templateUrl: "./server.component.html",
+  styleUrls: ["./server.component.css"]
+})
+
+export class ServerComponent {
+  serverId = 10 as number;
+  serverStatus = "offline" as string;
+
+}
