9f4fc5bcbd77e296b60299a7bc9bea7fc2c49d36
Fabio Stori <fabstx@gmail.com>
2020-07-27 00:00:46 -0300

    course-project

---
+++ course-project/src/app/recipes/recipes-details/recipes-details.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-recipes-details",
+  templateUrl: "./recipes-details.component.html",
+  styleUrls: ["./recipes-details.component.css"],
+})
+export class RecipesDetailsComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+}
