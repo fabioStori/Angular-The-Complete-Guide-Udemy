8ddd8ef83f95f4da6a52ff285df808ab9a49d6b7
Fabio Stori <f196631@dac.unicamp.br>
2020-08-05 16:12:14 -0300

    working on section 28

--- Section-28/testing-final/testing-final/src/app/user/user.component.spec.ts
+++ Section-28/testing-final/testing-final/src/app/user/user.component.spec.ts
@@ -1,25 +1,42 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { UserComponent } from './user.component';
+import { UserComponent } from "./user.component";
+import { UserService } from "./user.service";
 
-describe('UserComponent', () => {
-  let component: UserComponent;
-  let fixture: ComponentFixture<UserComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ UserComponent ]
-    })
-    .compileComponents();
-  }));
-
+describe("UserComponent", () => {
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      declarations: [UserComponent],
+    });
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it("should create the app", () => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy(); //check if exists
+  });
+  it("should use the user name from the service", () => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    let userService = fixture.debugElement.injector.get(UserService);
+    fixture.detectChanges();
+    expect(userService.user.name).toEqual(app.user.name);
+  });
+  it("should display the username if user is logged in", () => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    app.isLoggedIn = true;
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector("p").textContent).toContain(app.user.name);
+  });
+  it("should' t display the username if user is not logged in", () => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    app.isLoggedIn = false;
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector("p").textContent).not.toContain(
+      app.user.name
+    );
   });
 });
