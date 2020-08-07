02d732571edecacbb54deb9f2ba510d180f3d705
Fabio Stori <fabstx@gmail.com>
2020-08-06 23:13:13 -0300

    working on section 15

--- Section-15/forms-td-start/src/app/app.component.ts
+++ Section-15/forms-td-start/src/app/app.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  @ViewChild("f", { static: true }) signUpForm: NgForm;
+
+  defaultQuestion = "pet";
+  answer = "";
+  genders = ["male", "female"];
+  user = {
+    username: "",
+    email: "",
+    secretQuestion: "",
+    answer: "",
+    gender: "",
+  };
+  submitted: boolean = false;
+  subscriptions = ["Basic", "Advanced", "Pro"];
+  selectedSubscription = "Advanced";
+
+  onSubscription(){
+    console.log(this.selectedSubscription)
+  }
+
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    const suggestedName = "Superuser";
+    // this.signUpForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: "",
+    //   },
+    //   secret: "pet",
+    //   questionAnswer: "",
+    //   gender: "male",
+    // });
+    this.signUpForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
+  }
+  // onSubmit(form: NgForm){
+  //   console.log(form)
+  // }
+  onSubmit(form: NgForm) {
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
+    this.submitted = true;
+
+    this.signUpForm.reset();
   }
 }
