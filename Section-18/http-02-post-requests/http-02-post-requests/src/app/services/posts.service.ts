fd65b3674901c66fd3e3a427263d57168c6abce1
Fabio Stori <f196631@dac.unicamp.br>
2020-08-11 13:08:38 -0300

    working on section 18

---
+++ Section-18/http-02-post-requests/http-02-post-requests/src/app/services/posts.service.ts
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Post } from '../models/post.model';
+import { map, catchError } from 'rxjs/operators';
+import { Observable, Subject, throwError } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class PostsService {
+  error = new Subject();
+
+  constructor(private http: HttpClient) {}
+
+  createAndStorePost(title: string, content: string): void {
+    const postData: Post = { title: title, content: content };
+    this.http
+      .post<{ name: string }>(
+        'https://ng-complete-guide-3cd1d.firebaseio.com/posts.json',
+        postData,
+        {
+          observe: 'response'
+        }
+      )
+      .subscribe(
+        (responseData) => {
+          console.log(responseData);
+        },
+        (error) => {
+          this.error = error['message'];
+        }
+      );
+  }
+
+  fetchPosts(): Observable<{}> {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
+    return this.http
+      .get<{ [key: string]: Post }>(
+        'https://ng-complete-guide-3cd1d.firebaseio.com/posts.json',
+        {
+          headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+          params: searchParams
+        }
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key))
+              postsArray.push({ ...responseData[key], id: key });
+          }
+          return postsArray;
+        }),
+        catchError((errorRes) => {
+          return throwError(errorRes);
+        })
+      );
+  }
+
+  deletePosts(): Observable<{}> {
+    return this.http.delete(
+      'https://ng-complete-guide-3cd1d.firebaseio.com/posts.json'
+    );
+  }
+}
