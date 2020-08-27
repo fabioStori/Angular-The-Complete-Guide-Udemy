bb26859498cb0e9daf30e832720178726c3b0062
Fabio Stori <fstori@vmware.com>
2020-08-27 14:09:58 -0300

    working on course project forms

--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -1,23 +1,26 @@
-import { Injectable } from "@angular/core";
-import { Recipe } from "./recipe.model";
-import { Ingredient } from "../shared/ingredient.model";
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
   private recipes: Recipe[] = [
     new Recipe(
-      "Carne",
-      "Tomate",
-      "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg",
-      [new Ingredient("Meat", 1), new Ingredient("Frieds", 20)]
+      'Carne',
+      'Tomate',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('Frieds', 20)]
     ),
     new Recipe(
-      "Burger",
-      "Quejo",
-      "https://media.gazetadopovo.com.br/2019/11/20171835/HAMBURGUERIA-AGUA-VERDE_PE%CC%81-VERMEIO_RAFAEL_FESTIVAL-BURGER-FUNFEST_ESTU%CC%81DIO-A%CC%80-MILANESA-960x540.jpg",
-      [new Ingredient("Buns", 13), new Ingredient("Mear", 12)]
-    ),
+      'Burger',
+      'Quejo',
+      'https://media.gazetadopovo.com.br/2019/11/20171835/HAMBURGUERIA-AGUA-VERDE_PE%CC%81-VERMEIO_RAFAEL_FESTIVAL-BURGER-FUNFEST_ESTU%CC%81DIO-A%CC%80-MILANESA-960x540.jpg',
+      [new Ingredient('Buns', 13), new Ingredient('Mear', 12)]
+    )
   ];
 
   constructor(private slService: ShoppingListService) {}
@@ -32,5 +35,14 @@ export class RecipeService {
 
   addIngredientsToSL(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe): void {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+  updateRecipe(index: number, newRecipe: Recipe): void {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
--- course-project/src/app/recipes/recipes-list/recipes-list.component.ts
+++ course-project/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from "@angular/core";
-import { Recipe } from "../recipe.model";
-import { RecipeService } from "../recipe.service";
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: "app-recipes-list",
-  templateUrl: "./recipes-list.component.html",
-  styleUrls: ["./recipes-list.component.css"],
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) {}
+  constructor(
+    private recipeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.recipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
+      this.recipes = recipes;
+    });
     this.recipes = this.recipeService.getRecipes(); //getting copy of the original array
   }
-  onNewRecipe(){
-    this.router.navigate(['new'], {relativeTo: this.route})
+  onNewRecipe() {
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
 }
